547b51f18cd8ecf281b24e3c4f8fc50ba7bbd98c
Anas Sadek <anas.yogi99@gmail.com>
2025-11-28 20:25:36 -0500

    modified the model to support auto generated id instead of using static increments

--- lib/models/Car.dart
+++ lib/models/Car.dart
@@ -2,19 +2,14 @@ import 'package:floor/floor.dart';
 
 @entity
 class Car {
-  static int ID = 1;
-
   @primaryKey
-  int id;
+  final int? id;
+
   int year;
   String make;
   String model;
   double price;
   double kilometers;
 
-  Car(this.id, this.year, this.make, this.model, this.price, this.kilometers) {
-    if (this.id > ID) {
-      ID = this.id + 1;
-    }
-  }
+  Car(this.id, this.year, this.make, this.model, this.price, this.kilometers);
 }
