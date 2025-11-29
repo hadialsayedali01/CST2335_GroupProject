bcc072eb86da55f0ddd81d7a1b4b1bf269280495
Anas Sadek <anas.yogi99@gmail.com>
2025-11-28 21:28:16 -0500

    added support for ar in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -21,39 +21,34 @@ class MyApp extends StatefulWidget {
   }
 
   @override
-  MyAppState createState(){
+  MyAppState createState() {
     return MyAppState();
   }
 }
 
-class MyAppState extends State<MyApp>{
+class MyAppState extends State<MyApp> {
   var _locale = Locale("en", "CA");
-  void changeLanguage(Locale locale){
-    setState(
-        (){
-          _locale = locale;
-        }
-    );
+  void changeLanguage(Locale locale) {
+    setState(() {
+      _locale = locale;
+    });
   }
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
-      supportedLocales: [
-        Locale("en", "CA"),
-        Locale("fr")
-      ],
-      localizationsDelegates: const [ 
+      supportedLocales: [Locale("ar"), Locale("en", "CA"), Locale("fr")],
+      localizationsDelegates: const [
         AppLocalizations.delegate,
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
       ],
       locale: _locale,
       routes: {
-        'customerList' : (context) => CustomerListPage(),
-        'carsForSale' : (context) => CarsForSalePage(),
-        'boatsForSale' : (context) => BoatsForSalePage(),
-        'purchaseOffer' : (context) => PurchaseOfferPage()
+        'customerList': (context) => CustomerListPage(),
+        'carsForSale': (context) => CarsForSalePage(),
+        'boatsForSale': (context) => BoatsForSalePage(),
+        'purchaseOffer': (context) => PurchaseOfferPage(),
       },
       title: 'CST2355 Group Project',
       theme: ThemeData(
@@ -76,7 +71,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   var homeButtonPadding = 5.0;
   var homeButtonTextStyle = TextStyle(fontSize: 30, color: Colors.black);
   var homeButtonFixedWidth = 300.0;
@@ -84,30 +78,30 @@ class _MyHomePageState extends State<MyHomePage> {
   /// This method returns a formatted button that leads to one of the four application pages.
   /// It takes a String routeName parameter for the name of the route to navigate to.
   /// It takes a String buttonText parameter for the displayed text on the button.
-  Widget? returnFormattedHomeButton(String? routeName, String? buttonText){
+  Widget? returnFormattedHomeButton(String? routeName, String? buttonText) {
     return Padding(
-        padding: EdgeInsets.all(homeButtonPadding),
-        child: Container(
-            width: homeButtonFixedWidth,
-            child:ElevatedButton(
-                onPressed: (){Navigator.pushNamed(context, routeName!);},
-                child: Text(buttonText!, style: homeButtonTextStyle)
-            )
-        )
+      padding: EdgeInsets.all(homeButtonPadding),
+      child: Container(
+        width: homeButtonFixedWidth,
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pushNamed(context, routeName!);
+          },
+          child: Text(buttonText!, style: homeButtonTextStyle),
+        ),
+      ),
     );
   }
 
   /// This method returns a circular-formatted image.
   /// It takes a String imageSource parameter that is the path of the image.
-  Widget? returnFormattedImage(String? imageSource){
+  Widget? returnFormattedImage(String? imageSource) {
     return Padding(
       padding: EdgeInsets.all(15),
       child: CircleAvatar(
-          radius: 150,
-          backgroundImage: AssetImage(
-              imageSource!
-          )
-      )
+        radius: 150,
+        backgroundImage: AssetImage(imageSource!),
+      ),
     );
   }
 
@@ -125,28 +119,28 @@ class _MyHomePageState extends State<MyHomePage> {
             // images row
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children:[
+              children: [
                 returnFormattedImage('images/boat.jpg')!,
-                returnFormattedImage('images/car.jpg')!
-              ]
+                returnFormattedImage('images/car.jpg')!,
+              ],
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                returnFormattedHomeButton("boatsForSale" , "Boat List")!,
-                returnFormattedHomeButton("carsForSale" , "Car List")!,
-              ]
+                returnFormattedHomeButton("boatsForSale", "Boat List")!,
+                returnFormattedHomeButton("carsForSale", "Car List")!,
+              ],
             ),
             Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  returnFormattedHomeButton("customerList" , "Customer List")!,
-                  returnFormattedHomeButton("purchaseOffer" , "Purchase Offer")!
-                ]
-            )
-          ]
-        )
-      )
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                returnFormattedHomeButton("customerList", "Customer List")!,
+                returnFormattedHomeButton("purchaseOffer", "Purchase Offer")!,
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
